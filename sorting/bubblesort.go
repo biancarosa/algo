1dbf99725decb393f5ff2018fb25a8303f568277
Bianca Rosa <me@biancarosa.com.br>
2023-03-11 11:18:46 -0300

    adiciona ordenação para tipos float

--- sorting/bubblesort.go
+++ sorting/bubblesort.go
@@ -1,8 +1,21 @@
 package sorting
 
-type BubbleSort struct{}
+type BubbleSort struct{} // classe
 
-func (bs *BubbleSort) Sort(v []int) []int {
+func (bs *BubbleSort) Sort(v []int) []int { // metodo
+	for i, a := range v {
+		for j, b := range v {
+			if b > a {
+				tmp := v[i]
+				v[i] = v[j]
+				v[j] = tmp
+			}
+		}
+	}
+	return v
+}
+
+func (bs *BubbleSort) SortFloat(v []float64) []float64 { // metodo
 	for i, a := range v {
 		for j, b := range v {
 			if b > a {
--- sorting/bubblesort_test.go
+++ sorting/bubblesort_test.go
@@ -8,8 +8,22 @@ import (
 
 func TestBubbleSort(t *testing.T) {
 	bs := BubbleSort{}
-	sorted := bs.Sort([]int{1, 3, 2})
-	assert.Equal(t, []int{1, 2, 3}, sorted)
+	notSorted := []int{1, 3, 2}
+	sorted := bs.Sort(notSorted)
+	expectedSorted := []int{1, 2, 3}
+	assert.Equal(t, expectedSorted, sorted)
+}
+
+func TestBubbleSortFloat(t *testing.T) {
+	obj := new(BubbleSort)
+
+	notSorted := []float64{5.5, 0.5, 1.2, 6.7, 0.3, 9.8}
+
+	sorted := obj.SortFloat(notSorted)
+
+	expected := []float64{0.3, 0.5, 1.2, 5.5, 6.7, 9.8}
+
+	assert.Equal(t, expected, sorted)
 }
 
 func BenchmarkBubbleSort(b *testing.B) {
